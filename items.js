4f4135bbdb1ecd6b1b636dc5fc6e12995cef6516
Sarath Raj <sarathrj10@gmail.com>
2020-08-12 20:38:14 +0530

    Item page compleated

---
+++ items.js
@@ -0,0 +1,39 @@
+const items =[
+    {
+        id : 1,
+        name :'Redmi K30',
+        price : 12000,
+        quantity : 50
+    },
+    {
+        id : 2,
+        name :'Samsung Galaxy',
+        price : 15000,
+        quantity : 20
+    },
+    {
+        id : 3,
+        name :'Vivo Y!',
+        price : 20000,
+        quantity : 30
+    },
+    {
+        id : 4,
+        name :'Lava Z16',
+        price : 10000,
+        quantity : 10
+    },
+    {
+        id : 5,
+        name :'HTC Wildfire',
+        price : 25000,
+        quantity : 20
+    },
+    {
+        id : 6,
+        name :'Iphone X',
+        price : 50000,
+        quantity : 10
+    }
+];
+module.exports = items;
